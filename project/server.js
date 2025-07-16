7dd03b92403ace9e5bbd05dc9ecf3ecf5bec198f
Lyndsay.may <Lyndsay.may@libertymutual.com>
2025-07-16 14:26:49 -0400

    completed stage07 add put endpoint

--- project/server.js
+++ project/server.js
@@ -35,7 +35,7 @@ app.get("/reset", async (req, res) => {
 
 app.post('/customers', async (req, res) => {
     const newCustomer = req.body;
-    if (newCustomer === null || req.body != {}) {
+    if (newCustomer === null || req.body == {}) {
         res.status(400);
         res.send("missing request body");
     } else {
@@ -62,4 +62,23 @@ app.get("/customers/:id", async (req, res) => {
          res.status(404);
          res.send(err);
      }   
+});
+
+app.put('/customers/:id', async (req, res) => {
+    const id = req.params.id;
+    const updatedCustomer = req.body;
+    if (updatedCustomer === null || req.body == {}) {
+        res.status(400);
+        res.send("missing request body");
+    } else {
+        delete updatedCustomer._id;
+        // return array format [message, errMessage]
+        const [message, errMessage] = await da.updateCustomer(updatedCustomer);
+        if (message) {
+            res.send(message);
+        } else {
+            res.status(400);
+            res.send(errMessage);
+        }
+    }
 });
